ba109b1f4beb7ffe06fa34e2e4ba6632c94befaa
moeinheydaridev <moeinheydaridev@gmail.com>
2024-08-04 16:37:24 +0330

    Add files via upload

---
+++ Async Await/Async Await/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        var CapturedeValue = i;
+        ThreadPool.QueueUserWorkItem(delegate
+        {
+            Console.WriteLine(CapturedeValue);
+            Thread.Sleep(10000);
+        });
+    }
+}
+Console.ReadLine();
+static class MyThreadPool
+{
+    private static readonly BlockingCollection<Action> s_WorkItem = new();
+    public static void QueueUserWorkItem(Action action) => s_WorkItem.Add(action);
+    static MyThreadPool()
+    {
+        for (var i = 0; i < Environment.ProcessorCount; i++)
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    new Thread(() =>
+                    {
+                        Action WorkItem = s_WorkItem.Take();
+                        WorkItem();
+
+                    })
+                    { IsBackground = true }.Start();
+                }
+            });
+            }
+    }
+}
+            
+            
+         
+            
+            
+    
